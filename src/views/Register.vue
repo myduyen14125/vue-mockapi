75b71d770ad3136346c077b4b4e237ad36e6b341
yuu <myduyen14125@gmail.com>
2021-07-05 00:39:21 +0700

    validate password

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,109 +2,131 @@
   <div class="register">
     <div class="register-container">
       <h1>Register</h1>
-      
-      <form action=""  v-on:submit.prevent="postUser">
+
+      <form action="" v-on:submit.prevent="postUser">
         <p style="text-align: left">* Create new account</p>
-        <input type="text" placeholder="Username" required v-model="username">
-        <input type="password" placeholder="Password" minlength="6" required v-model="password">
-        <span style="color: red" v-bind:class="{strongPassword}">Password needs at least 6 characters</span>
+        <input type="text" placeholder="Username" required v-model="username" />
+        <input
+          type="password"
+          placeholder="Password"
+          minlength="6"
+          required
+          v-model="password"
+        />
+        <span style="color: red" v-bind:class="{ strongPassword }"
+          >Password needs at least 1 number or special character</span
+        >
 
         <p style="text-align: left">* Personal information</p>
-        <input type="text" placeholder="Fullname" required v-model="name">
-        <input type="text" placeholder="Phone Number" required v-model="phoneNumber">
+        <input type="text" placeholder="Fullname" required v-model="name" />
+        <input
+          type="text"
+          placeholder="Phone Number"
+          required
+          v-model="phoneNumber"
+        />
 
         <div class="more">
           <div class="checkbox">
-            <input type="checkbox" required>
+            <input type="checkbox" required />
             <span>I agree to your terms and conditions.</span>
           </div>
         </div>
-      <button>Register</button>
+        <button>Register</button>
       </form>
 
       <div class="redirect-box">
-        <p>Have an account? </p>
+        <p>Have an account?</p>
         <router-link to="/login">Login here</router-link>
       </div>
     </div>
   </div>
-  
 </template>
 
 <script>
-import axios from 'axios'
-export default ({
-  name: 'Register',
-  components: {
-  
-  },
+import axios from "axios";
+export default {
+  name: "Register",
+  components: {},
   created() {
-    console.log(this.$route)
-  }
-  ,
+    console.log(this.$route);
+  },
   data() {
     return {
-      username: '',
-      password: '',
-      name: '',
-      phoneNumber: '',
+      username: "",
+      password: "",
+      name: "",
+      phoneNumber: "",
       strongPassword: true,
-    }
+    };
   },
   methods: {
-    checkPasswd(){
-      if(this.password.length < 6){
-        this.strongPassword == false
+    validPassword(){
+      let check = 0;
+      for(let i = 0; i < this.password.length; i++){
+        if(this.password[i] > '0' && this.password[i] < '9'){
+          this.strongPassword = true;
+          console.log(this.strongPassword)
+          check = 1;
+        }
       }
+      if(check === 0){
+        this.strongPassword = false;
+      } 
+      console.log("check = "+ check)
+    },
+    async checkAccount(){
+
     },
     async postUser() {
-      await axios.post('https://60d94868eec56d001747768f.mockapi.io/v1/users', {
-        username: this.username,
-        password: this.password,
-        name: this.name,
-        phoneNumber: this.phoneNumber,
-        
-      });
-      alert('You have successfully registered !')
-      this.$router.push(`/users`)
+      this.validPassword();
+      if(this.strongPassword === true) {
+        await axios.post('https://60d94868eec56d001747768f.mockapi.io/v1/users', {
+          username: this.username,
+          password: this.password,
+          name: this.name,
+          phoneNumber: this.phoneNumber,
+          
+        });
+        alert('You have successfully registered !')
+        this.$router.push(`/users`)
+      }
+      else{
+        this.password = ''
+      }
     }
   },
   
-  checkAccount(){
-
-  }
-  
-})
+};
 </script>
 
 <style scoped>
-a{
+a {
   color: #2c3e50;
   border-bottom: 1px solid white;
   transition: 0.2s;
 }
-a:active{
+a:active {
   color: #1e2731;
 }
-a:hover{
-    border-bottom: 1px solid #2c3e50;
+a:hover {
+  border-bottom: 1px solid #2c3e50;
 }
 
-.register{
+.register {
   display: flex;
   justify-content: center;
   align-items: center;
   height: 85vh;
 }
-.register-container{
+.register-container {
   width: 450px;
   padding: 1rem;
   margin: auto;
   text-align: center;
-  box-shadow: 5px 5px 13px -4px rgba(0,0,0,0.71);
-
+  box-shadow: 5px 5px 13px -4px rgba(0, 0, 0, 0.71);
 }
-input{
+input {
   display: block;
   padding: 15px;
   border-radius: 10px;
@@ -118,10 +140,10 @@ input{
   transition: 0.4s;
   border: 1px solid white;
 }
-input:hover{
+input:hover {
   border: 1px solid gray;
 }
-button{
+button {
   width: 100%;
   padding: 15px;
   font-size: 1.15rem;
@@ -132,61 +154,59 @@ button{
   transition: 0.4s;
   cursor: pointer;
 }
-button:hover{
+button:hover {
   background-color: #1e2731;
   color: white;
 }
-.more{
+.more {
   display: flex;
   justify-content: flex-start;
   align-items: center;
   font-size: 0.9rem;
 }
-  .checkbox{
-    display: flex;
-    align-items: center;
-  }
+.checkbox {
+  display: flex;
+  align-items: center;
+}
 
-input[type="checkbox"]{
+input[type="checkbox"] {
   width: fit-content;
   margin: 10px;
 }
 
-.redirect-box{
+.redirect-box {
   display: flex;
   align-items: center;
   justify-content: space-between;
 }
 
 /* Dynamic css */
-.strongPassword{
+.strongPassword {
   display: none;
 }
 
 @media screen and (max-width: 1140px) {
-  .content{
+  .content {
     font-size: 0.9rem;
   }
-  h1{
+  h1 {
     font-size: 1.5rem;
   }
 }
 @media screen and (max-width: 992px) {
-  .intro p{
+  .intro p {
     width: 60%;
   }
 }
 @media screen and (max-width: 768px) {
-  .intro p{
+  .intro p {
     width: 80%;
   }
 }
 @media screen and (max-width: 576px) {
-  .intro p{
+  .intro p {
     width: 80%;
     font-size: 1.1rem;
   }
-
 }
-
 </style>
