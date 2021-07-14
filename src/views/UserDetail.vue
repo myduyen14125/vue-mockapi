de689e6152a734c53fcdfbc73ef927a772d76acd
yuu <myduyen14125@gmail.com>
2021-07-14 20:31:42 +0700

    .

--- src/views/UserDetail.vue
+++ src/views/UserDetail.vue
@@ -2,139 +2,147 @@
   <div class="user-detail">
     <!-- <h1>{{$route.params.id}}</h1> -->
     <div class="cover">
-      <Author v-bind:user='user'/>
+      <Author v-bind:user="user" />
       <div class="edit-profile">
         <button class="big-edit" @click="editProfile">Edit Profile</button>
-        <div class="menu-edit" v-bind:class="{showMenuEdit: availableMenuEdit}">
+        <div
+          class="menu-edit"
+          v-bind:class="{ showMenuEdit: availableMenuEdit }"
+        >
           <button @click="editName">Edit Your Name</button>
           <button @click="editPhone">Edit Phone Number</button>
           <button @click="deleteUser()">Delete Account</button>
-          <hr style="margin: 1px">
+          <hr style="margin: 1px" />
           <button @click="cancelEdit">Cancel</button>
         </div>
-      </div>     
+      </div>
 
-      <div class="edit-modal" v-bind:class="{showModal: availableModal}">
+      <div class="edit-modal" v-bind:class="{ showModal: availableModal }">
         <div class="modal-content">
           <h3>Edit your information below here !</h3>
-          <hr>
-          <form action=""  v-on:submit.prevent="successEdit(user.id)">
-            <input v-model="currentInput" type="text" required>
+          <hr />
+          <form action="" v-on:submit.prevent="successEdit(user.id)">
+            <input v-model="currentInput" type="text" required />
             <button type="submit">Submit</button>
             <button @click="cancelModal">Cancel</button>
           </form>
-
         </div>
       </div>
     </div>
-    
-    <div class="container">      
-      <p>{{user.information}}</p><br>
-      <p>{{user.information}}</p><br>
-      <p>{{user.information}}</p><br>
-      
+
+    <div class="container">
+      <p>{{ user.information }}</p>
+      <br />
+      <p>{{ user.information }}</p>
+      <br />
+      <p>{{ user.information }}</p>
+      <br />
     </div>
-    
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-import Author from '../components/uncommon/Author.vue'
+import axios from "axios";
+import Author from "../components/uncommon/Author.vue";
 
 export default {
   components: {
-    Author
+    Author,
   },
   data() {
     return {
-      user: {},
       currentInput: null,
-      edit: '',
+      edit: "",
       availableModal: false,
-      availableMenuEdit: false
-    }
-    
+      availableMenuEdit: false,
+    };
   },
-  async created() {
-    const response = await axios.get(`https://60d94868eec56d001747768f.mockapi.io/v1/users/${this.$route.params.id}`)
-    this.user = response.data
-    console.log(response)
+  computed: {
+    user() {
+      let activeUser = {};
+      this.$store.state.persons.forEach((user) => {
+        if (user.id == this.$route.params.id) activeUser = user;
+      });
+      return activeUser;
+    },
   },
   methods: {
-    editProfile(){
-      this.availableMenuEdit = true
+    editProfile() {
+      this.availableMenuEdit = true;
     },
-    editName(){
-      this.edit = 'name';
-      this.availableModal = true
-      this.availableMenuEdit = false
+    editName() {
+      this.edit = "name";
+      this.availableModal = true;
+      this.availableMenuEdit = false;
     },
-    editPhone(){
-      this.edit = 'phone';
-      this.availableModal = true
-      this.availableMenuEdit = false
+    editPhone() {
+      this.edit = "phone";
+      this.availableModal = true;
+      this.availableMenuEdit = false;
     },
-    async successEdit(id){
-      if(this.edit === 'name'){
+    async successEdit(id) {
+      if (this.edit === "name") {
         await axios
-        .put(`https://60d94868eec56d001747768f.mockapi.io/v1/users/${id}`,
-          {
+          .put(`https://60d94868eec56d001747768f.mockapi.io/v1/users/${id}`, {
             name: this.currentInput,
           })
-        .then(function (response){
-          alert('Success Updated!')
-          // const response = await axios.get(`https://60d94868eec56d001747768f.mockapi.io/v1/users/${id}`)
-          // location.reload();
-          console.log(response)
-          console.log(response.data)
-        })
-        .catch(function (error){
-          console.log(error)
-          alert('Error !')
-        })
-        const response = await axios.get(`https://60d94868eec56d001747768f.mockapi.io/v1/users/${id}`)
-        console.log(response.data)
-        this.user = response.data
+          .then(function(response) {
+            alert("Success Updated!");
+            // const response = await axios.get(`https://60d94868eec56d001747768f.mockapi.io/v1/users/${id}`)
+            // location.reload();
+            console.log(response);
+            console.log(response.data);
+          })
+          .catch(function(error) {
+            console.log(error);
+            alert("Error !");
+          });
+        const response = await axios.get(
+          `https://60d94868eec56d001747768f.mockapi.io/v1/users/${id}`
+        );
+        console.log(response.data);
+        this.user = response.data;
       }
-      if(this.edit === 'phone'){
-        await axios.put(`https://60d94868eec56d001747768f.mockapi.io/v1/users/${id}`,
-        {
-          phoneNumber: this.currentInput,
-        })
-        alert('Success Updated!')
-        this.user.phoneNumber = this.currentInput
+      if (this.edit === "phone") {
+        await axios.put(
+          `https://60d94868eec56d001747768f.mockapi.io/v1/users/${id}`,
+          {
+            phoneNumber: this.currentInput,
+          }
+        );
+        alert("Success Updated!");
+        this.user.phoneNumber = this.currentInput;
         //Another way for a faster user experience
       }
-      this.currentInput = ''
-      this.availableModal = false
+      this.currentInput = "";
+      this.availableModal = false;
     },
-    cancelEdit(){
-      this.availableMenuEdit = false
+    cancelEdit() {
+      this.availableMenuEdit = false;
     },
-    cancelModal(){
-      this.availableModal = false
+    cancelModal() {
+      this.availableModal = false;
     },
-    deleteUser(){
-      this.$store.commit('deleteUser', this.user)
-      alert('Success !')
+    deleteUser() {
+      this.$store.commit("deleteUser", this.user);
+      alert("Success !");
       this.$router.push(`/users`);
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped>
-.user-detail{
+.user-detail {
   margin: 0;
   min-height: 30vh;
 }
-.container{
+.container {
   width: 55%;
   text-align: left;
   margin: 50px auto;
 }
-button{
+button {
   margin-right: 15px;
   background-color: white;
   padding: 8px 15px;
@@ -144,55 +152,55 @@ button{
   transition: 0.4s;
   cursor: pointer;
 }
-button:hover{
+button:hover {
   background-color: #1e2731;
   color: white;
 }
-.cover{
+.cover {
   position: relative;
   padding: 0;
   height: 50vh;
   background: linear-gradient(180deg, #f2f2f2, #ffffff);
   padding-bottom: 50px;
   box-sizing: border-box;
-  box-shadow: 3px 3px 5px -4px rgba(0,0,0,0.71);
+  box-shadow: 3px 3px 5px -4px rgba(0, 0, 0, 0.71);
 }
-.author{
+.author {
   margin: auto;
 }
-.edit-profile{
+.edit-profile {
   position: absolute;
   top: 87%;
-  right: calc(50% - 230px /2);
+  right: calc(50% - 230px / 2);
   width: 230px;
   padding: 0;
 }
-  .big-edit{
-    width: 150px;
-    margin: auto;
-    border: 1px solid gray;
-  }
-  .menu-edit{
-    width: 100%;
-    padding: 5px;
-    border: 1px solid gray;
-    border-radius: 5px;
-    background-color: white;
-    box-sizing: border-box;
-    display: none;
-    transition: 0.3s;
-    box-shadow: 3px 4px 10px -5px rgba(0,0,0,0.71);
-  }
-  .showMenuEdit{
-    display: block;
-  }
-    .menu-edit button{
-      width: 100%;
-      text-align: left;
-    }
+.big-edit {
+  width: 150px;
+  margin: auto;
+  border: 1px solid gray;
+}
+.menu-edit {
+  width: 100%;
+  padding: 5px;
+  border: 1px solid gray;
+  border-radius: 5px;
+  background-color: white;
+  box-sizing: border-box;
+  display: none;
+  transition: 0.3s;
+  box-shadow: 3px 4px 10px -5px rgba(0, 0, 0, 0.71);
+}
+.showMenuEdit {
+  display: block;
+}
+.menu-edit button {
+  width: 100%;
+  text-align: left;
+}
 
 /* Modal */
-.edit-modal{
+.edit-modal {
   display: none;
   position: fixed;
   z-index: 1;
@@ -205,37 +213,37 @@ button:hover{
 .showModal {
   display: block;
 }
-  .modal-content{
-    margin: 12% auto;
-    padding: 10px;
-    width: 400px;
-    box-shadow: 0 5px 8px 0 rgba(0, 0, 0, 0.2), 0 7px 20px 0 rgba(0, 0, 0, 0.17);
-    background-color: white;
-    border-radius: 6px;
-    animation-name: modalopen;
-  }
-  input{
-    display: block;
-    padding: 15px;
-    border-radius: 10px;
-    background-color: #f3f3f3;
-    color: #2c3e50;
-    width: 90%;
-    margin: 20px auto;
-    border: none;
-    box-sizing: border-box;
-    transition: 0.4s;
-    border: 1px solid white;
-  }
-  input:hover{
-    border: 1px solid gray;
-  }
-  .modal-content button{
-    border: 1px solid #2c3e50;
-  }
+.modal-content {
+  margin: 12% auto;
+  padding: 10px;
+  width: 400px;
+  box-shadow: 0 5px 8px 0 rgba(0, 0, 0, 0.2), 0 7px 20px 0 rgba(0, 0, 0, 0.17);
+  background-color: white;
+  border-radius: 6px;
+  animation-name: modalopen;
+}
+input {
+  display: block;
+  padding: 15px;
+  border-radius: 10px;
+  background-color: #f3f3f3;
+  color: #2c3e50;
+  width: 90%;
+  margin: 20px auto;
+  border: none;
+  box-sizing: border-box;
+  transition: 0.4s;
+  border: 1px solid white;
+}
+input:hover {
+  border: 1px solid gray;
+}
+.modal-content button {
+  border: 1px solid #2c3e50;
+}
 
 @media screen and (max-width: 992px) {
-  .container{
+  .container {
     width: 75%;
     text-align: left;
     margin: 50px auto;
